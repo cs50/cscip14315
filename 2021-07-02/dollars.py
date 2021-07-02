2166c59c49f1c41350dc370026a8b1c6fbe9d760
David J. Malan <malan@harvard.edu>
2021-07-02 09:53:47 -0400

    added dollars

---
+++ 2021-07-02/dollars.py
@@ -0,0 +1,19 @@
+def validate_amount(amount):
+
+    if not amount.startswith("$"):
+        return False
+    amount = amount[1:]
+
+    if len(amount) < 4:
+        return False
+
+    if not amount[-1].isdigit() or not amount[-2].isdigit():
+        return False
+
+    if amount[-3] != ".":
+        return False
+
+    if not amount[:-3].isdigit():
+        return False
+
+    return True
