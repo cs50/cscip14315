82555119deb6b819b51731c29c9ececbd666430e
Kareem Zidane <kzidane@cs50.harvard.edu>
2021-07-09 09:41:05 -0400

    Update gui5.py

--- 2021-07-09/gui5.py
+++ 2021-07-09/gui5.py
@@ -7,12 +7,16 @@ def main():
     window.geometry("200x200")
 
     button = tk.Button(window, text="Click me")
+    button["command"] = greet
     button.grid(row=0, column=0)
 
     label = tk.Label(window, text="This is a label")
     label.grid(row=1, column=0)
 
     window.mainloop()
+
+def greet():
+    print("Hi there")
 
 if __name__ == "__main__":
     main()
